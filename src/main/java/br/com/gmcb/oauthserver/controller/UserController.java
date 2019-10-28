4a4d1be9d0b750d7d61937b08fa0d8461a1ef4fb
gmadalon <gustavo.madalon@gmail.com>
2019-10-28 04:43:27 +0100

    Inclusao de usuario

--- src/main/java/br/com/gmcb/oauthserver/controller/UserController.java
+++ src/main/java/br/com/gmcb/oauthserver/controller/UserController.java
@@ -1,19 +1,27 @@
 package br.com.gmcb.oauthserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.gmcb.oauthserver.dto.UserDTO;
+import br.com.gmcb.oauthserver.service.user.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService){
+		this.userService = userService;
+	}
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public void addUser(@RequestBody UserDTO userDTO) {
-		
-		
+		userService.addUser(userDTO.getEmail(), userDTO.getPassword());
 	}
 }
